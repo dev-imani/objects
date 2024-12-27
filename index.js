b9d6af1377ea694aed6ce50db9d41d4e6e451301
Kel Kel <michaelimani95@gmail.com>
2024-12-27 18:13:01 +0300

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const name = window.prompt("whats your name")
-console.log(name)
+//  // day one off learning objects
+//  const obj1={
+//     alive:true,
+//     answer:42,
+//     hobies:["eat","drink","sleep"],
+//     schools:{
+//         Nairobi:"The Alliance",
+//         Coastal:"Mombasa Boys",
+//         Western:"Chavakali Boys",
+//     },
+//     function:function(){
+//         return "i miss home"
+//     },
+//     action: function () {
+//         return `i am ${this.answer} years old and i had a preveledge of studying at ${this.schools.Nairobi} high school`}
+// }
+
+// console.log(obj1.schools.Western)
+// console.log(obj1.action())
+// console.log(obj1.hobies[1])
+// console.log(obj1["schools"])
+// console.log(obj1.function())
